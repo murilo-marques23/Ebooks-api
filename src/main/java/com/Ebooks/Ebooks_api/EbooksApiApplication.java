c1d89d67ccfadf327e61df51bd9a3e055a724152
murilo-marques23 <murilomendesm4@gmail.com>
2024-10-10 21:51:03 -0300

    Primeiro Commit

---
+++ src/main/java/com/Ebooks/Ebooks_api/EbooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.Ebooks.Ebooks_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbooksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ebooks/Ebooks_api/EbooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ebooks.Ebooks_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
